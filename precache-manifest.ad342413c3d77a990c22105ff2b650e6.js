2ca8de00381c2f420305bc7b589bf93db2fdb244
uchennaihe <urchihe@gmail.com>
2020-05-16 15:32:34 +0100

    Updates

---
+++ precache-manifest.ad342413c3d77a990c22105ff2b650e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f267e12e3e40987ab98dd01493373943",
+    "url": "/uchenna-ihe/index.html"
+  },
+  {
+    "revision": "bd6584e85bddd33bdd10",
+    "url": "/uchenna-ihe/static/css/main.a6c14227.chunk.css"
+  },
+  {
+    "revision": "22a94580e5cc7216cbae",
+    "url": "/uchenna-ihe/static/js/2.31ee3788.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/uchenna-ihe/static/js/2.31ee3788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd6584e85bddd33bdd10",
+    "url": "/uchenna-ihe/static/js/main.aa0b1beb.chunk.js"
+  },
+  {
+    "revision": "ce2e23c2a8ada3e976a2",
+    "url": "/uchenna-ihe/static/js/runtime-main.3b3b6f23.js"
+  },
+  {
+    "revision": "010e6b0331af89c6c802e7428e79a5c9",
+    "url": "/uchenna-ihe/static/media/Python.010e6b03.png"
+  },
+  {
+    "revision": "158dabdc6ccd036c57335fc969a4f370",
+    "url": "/uchenna-ihe/static/media/database.158dabdc.svg"
+  },
+  {
+    "revision": "9ebf3bafd80e90fbcab8397a5349e18a",
+    "url": "/uchenna-ihe/static/media/node.9ebf3baf.png"
+  },
+  {
+    "revision": "39dff610661b307f38a28b07ff4c8ec0",
+    "url": "/uchenna-ihe/static/media/php.39dff610.png"
+  },
+  {
+    "revision": "f7aecbdf6aaf85daac731d51c62c6ea2",
+    "url": "/uchenna-ihe/static/media/pic.f7aecbdf.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uchenna-ihe/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "a26fd6b245f8740732e41c455637a007",
+    "url": "/uchenna-ihe/static/media/ux-iu.a26fd6b2.png"
+  },
+  {
+    "revision": "d443c086950534cd2a77a057c628265e",
+    "url": "/uchenna-ihe/static/media/vue.d443c086.svg"
+  }
+]);
